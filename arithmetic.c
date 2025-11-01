c51480cbe18e8be592e2ed6690d9232854ef8ba8
Sagar2648 <145320621+Sagar2648@users.noreply.github.com>
2025-11-02 00:25:49 +0100

    Added arithmetic operators

---
+++ arithmetic.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+
+    int x = 10;
+    int y = 3;
+    int z = 0;
+
+    //z = x + y;
+    //z = x - y;
+    //z = x * y;
+    //z = x / y;
+    //z = x % y;
+
+    //x--;
+    x += 3;
+
+    //printf("The sum of x and y is %d\n", z);
+    printf(" %d\n", x);
+
+    return 0;
+}
