c610d507b4789aa86d4b22b89a083455e527e753
Sagar2648 <145320621+Sagar2648@users.noreply.github.com>
2025-11-05 11:23:15 +0100

    While loops in C

---
+++ whileLoops.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+
+int main() {
+
+    /* while loop = Continue some code WHILE the condition remains true
+                    Condition must be true for us to enter while loop */
+
+    /*char name[50] = "";
+
+    printf("Enter your name: ");
+    fgets(name, sizeof(name), stdin);
+    name[strlen(name) -1 ] = '\0';
+
+    while(strlen(name) == 0){
+        printf("Name cannot be empty please enter your name: ");
+        fgets(name, sizeof(name), stdin);
+        name[strlen(name) - 1] = '\0';
+    }
+
+    printf("Hello %s", name); */
+
+    bool isRunning = true;
+    char response = '\0';
+
+    do{
+        printf("You are playing a game\n");
+        printf("Would you like to continue? (Y = yes, N = no): ");
+        scanf(" %c", &response);
+
+        if(response != 'Y' && response != 'y'){
+            isRunning = false;
+        }
+    }while(isRunning);
+
+    printf("You exit the game.");
+    
+    return 0;
+}
