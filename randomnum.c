7f30dd5b49d1d10bcb761d984985505dda54e1e5
Sagar2648 <145320621+Sagar2648@users.noreply.github.com>
2025-11-05 12:10:47 +0100

    pseudo number and how to generate them in C

---
+++ randomnum.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(){
+
+    /* Pseudo-random = Appear random but are determined by a mathematical formula that uses a seed value 
+                       to generate a predictable sequence of numbers. advanced: Mersenne Twister or /dev/random */
+
+    srand(time(NULL));  // base seed is srand 
+
+    int min = 50;
+    int max = 100; 
+
+    int randomNum1 = (rand() % (max - min) + 1) + min;
+    int randomNum2 = (rand() % (max - min) + 1) + min;
+    int randomNum3 = (rand() % (max - min) + 1) + min;
+
+    printf("%d %d %d ", randomNum1, randomNum2, randomNum3);
+
+    return 0;
+}
