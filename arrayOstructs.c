6f6c2e537491591ed69eb884adb432655fa386ec
Sagar2648 <145320621+Sagar2648@users.noreply.github.com>
2025-11-05 20:47:23 +0100

    Array of struct in C

---
+++ arrayOstructs.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+typedef struct 
+{
+    char model[25];
+    int year;
+    int price;
+}Car;
+
+
+int main(){
+
+    /* array of structs = Array where each element contains a struct{}
+                          helps organize and groups together related data*/
+
+    Car cars[] = {{"Mustang", 2025, 32000}, {"Corvette", 2026, 68000}, {"Challenger", 2024, 29000}};
+
+    int number = sizeof(cars) / sizeof(cars[0]);
+
+    for (int i = 0; i < number; i++)
+    {
+        printf("Model: %s Year: %d Price: €%d\n", cars[i].model, cars[i].year, cars[i].price);
+    }
+
+    return 0;
+}
