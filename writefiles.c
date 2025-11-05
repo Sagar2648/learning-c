e043dad77d48442a17a5dc430b2d101dbf8735e4
Sagar2648 <145320621+Sagar2648@users.noreply.github.com>
2025-11-05 21:37:55 +0100

    Create and write a file in c

---
+++ writefiles.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(){
+
+    //WRITE A FILE
+
+    FILE *pFile = fopen("/Users/sagar/Desktop/output.txt", "w");
+
+    char text[] = "HELLO HELLO HELLO\n ROCKIN' EVERYWHERE!";
+
+    if(pFile == NULL){
+        printf("Error opening file\n");
+        return 1;
+    }
+
+    fprintf(pFile, "%s", text);
+
+    printf("File was written successfully!\n");
+
+    fclose(pFile);
+
+    return 0;
+}
